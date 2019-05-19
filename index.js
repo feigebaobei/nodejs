1a96dc696d8469e0269d3ef52ba08074b76e9fb9
feigebaobei <18515195415@163.com>
2019-05-19 17:56:48 +0800

    学完node的异步说明代码

--- index.js
+++ index.js
@@ -5,12 +5,21 @@
 var rect = require('./rectangle')
 function solveRect(l, b) {
   console.log(`Solveing for rectangle with l = ${l} and b = ${b}`)
-  if (l <= 0 || b <= 0) {
-    console.log(`Rectangle dimensions should be greater than zero: l = ${l}, and b = ${b}`)
-  } else {
-    console.log(`The area of the rectangle is ${rect.area(l, b)}`)
-    console.log(`The perimeter of the rectangle is ${rect.perimeter(l, b)}`)
-  }
+  // if (l <= 0 || b <= 0) {
+  //   console.log(`Rectangle dimensions should be greater than zero: l = ${l}, and b = ${b}`)
+  // } else {
+  //   console.log(`The area of the rectangle is ${rect.area(l, b)}`)
+  //   console.log(`The perimeter of the rectangle is ${rect.perimeter(l, b)}`)
+  // }
+
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log('error', err.message)
+    } else {
+      console.log(`area: ${rectangle.area()}`)
+      console.log(`perimeter: ${rectangle.perimeter()}`)
+    }
+  })
 }
 
 solveRect(2, 4)
--- rectangle.js
+++ rectangle.js
@@ -1,7 +1,22 @@
 // exports.perimeter = (x, y) => (2 * (x + y))
 // exports.area = (x, y) => (x * y)
 
-module.exports = {
-  perimeter: (x, y) => (2 * (x + y)),
-  area: (x, y) => (x * y)
+// module.exports = {
+//   perimeter: (x, y) => (2 * (x + y)),
+//   area: (x, y) => (x * y)
+// }
+
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    setTimeout(() => {
+      callback(new Error(`最小边不能小于0，l = ${x}, b = ${y}`), null), 2000
+    })
+  } else {
+    setTimeout(() => {
+      callback(null, {
+        perimeter: () => (2 * (x + y)),
+        area: () => (x * y)
+      }), 2000
+    })
+  }
 }
