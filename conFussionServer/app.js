79ed3c904e5383f9377cdb2fec536753765c634d
feigebaobei <18515195415@163.com>
2019-07-04 07:01:31 +0800

    不能通过验证

--- conFussionServer/app.js
+++ conFussionServer/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport')
+var authenticate = require('./authenticate')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -38,6 +40,9 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -78,22 +83,15 @@ app.use('/users', usersRouter);
 //   }
 // }
 function auth(req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
+    if (!req.user) {
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
+      err.status = 403;
+      // return next(err);
+      next(err)
     } else {
-        if (req.session.user === 'authenticated') {
-            console.log('req.session: ', req.session);
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            err.status = 401;
-            next(err);
-        }
+        next();
     }
 }
 
---
+++ conFussionServer/authenticate.js
@@ -0,0 +1,19 @@
+// var passport = require('passport'),
+//   LocalStrategy = require('passport-local').Strategy,
+//   User = require('./models/user')
+
+// exports.local = passport.use(new LocalStrategy(User.authenticate()))
+// passport.serializeUser(User.serializeUser())
+// passport.deserializeUser(User.deserializeUser())
+
+
+
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()))
+// passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
