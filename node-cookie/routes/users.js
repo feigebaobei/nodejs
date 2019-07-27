c1241099704f2707865ed7dce27e51e286fc64e1
feigebaobei <18515195415@163.com>
2019-07-27 14:06:29 +0800

    learn cookie

--- node-cookie/routes/users.js
+++ node-cookie/routes/users.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// set cookie
+router.get('/setcookie', (req, res, next) => {
+  res.cookie('name', 'stone', {maxAge: 60000, httpOnly: true})
+  res.cookie('name1', 'stone', {maxAge: 60000, httpOnly: true})
+  res.cookie('name20', 'stone', {maxAge: 60000, httpOnly: true, signed: true})
+  res.cookie('name21', 'stone', {maxAge: 60000, httpOnly: true, signed: true})
+  res.send('success for set up')
+})
+
+// get cookie
+router.get('/getcookie', (req, res, next) => {
+  let o = {
+    unsign: req.cookies,
+    signed: req.signedCookies
+  }
+  res.json(o)
+})
+
 module.exports = router;
