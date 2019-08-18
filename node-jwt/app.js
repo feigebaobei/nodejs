0c31f6eb6938f0445b59ec5a4ed6f27b5a73d22d
feigebaobei <18515195415@163.com>
2019-08-18 11:56:41 +0800

    from node_passport

---
+++ node-jwt/app.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session')
+var FileStore = require('session-file-store')(session)
+var passport = require('passport')
+var authenticate = require('./authenticate')
+// 因使用三方验证需要使用在三方注册app，但是我还没有正式的url.所以先不做三方登录。
+// var authGithub = require('./authGithub')
+// var LocalStrategy = require('passport-local').Strategy
+// var User = require('./models/user')
+// passport.use(new LocalStrategy(User.authenticate()))
+// passport.serializeUser(User.serializeUser())
+// passport.deserializeUser(User.deserializeUser())
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var news = require('./routes/news');
+var error = require('./routes/error');
+
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost:27017/confusion'
+const connect = mongoose.connect(url, {useNewUrlParser: true, useCreateIndex: true})
+
+connect.then(db => {
+  console.log('Connect correct to server')
+}, err => {console.log(err)})
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+
+app.use(session({
+  name: 'session-id',
+  cookie: {maxAge: 10000},
+  secret: '12345-67890',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/error', error);
+
+// 使用session保存登录信息.
+// let authFn = (req, res, next) => {
+//   if (req.session.auth) {
+//     next()
+//   } else {
+//     var err = new Error('You are not authenticated!')
+//     err.status = 403
+//     next(err)
+//   }
+// }
+
+// app.use(authFn)
+// 使用使用passport使用验证是否登录.
+app.use((req, res, next) => {
+  console.log(req.session)
+  console.log(req.isAuthenticated())
+  if (req.isAuthenticated()) {
+    next()
+  } else {
+    res.redirect('/error')
+  }
+})
+app.use('/news', news)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
